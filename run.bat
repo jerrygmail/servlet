d1d7066bde9550aa2f7a066fd1a1de47bce4926e
jerrygmail <45858805+jerrygmail@users.noreply.github.com>
2019-05-16 23:48:15 +0800

    add testing files

---
+++ run.bat
@@ -0,0 +1,3 @@
+c:\java\bin\javac.exe -cp C:\Users\JerryManaloto\.jenkins\workspace\Demo\bin;C:\Users\JerryManaloto\.jenkins\workspace\Demo\lib\* jenkins_demo.java
+rem c:\java\bin\java.exe -cp C:\Users\JerryManaloto\.jenkins\workspace\Demo\bin;C:\Users\JerryManaloto\.jenkins\workspace\Demo\lib\*;.\ jenkins_demo
+c:\java\bin\java.exe -cp C:\Users\JerryManaloto\.jenkins\workspace\Demo\bin;C:\Users\JerryManaloto\.jenkins\workspace\Demo\lib\*;.\ org.testng.TestNG testng.xml
